7b0dd8c986366c4c0f7742dadeaca9c00e449539
Eric Scrivner <eric@grapheffect.com>
2012-12-19 15:44:18 -0800

    Fix hackish groovy scanner not parsing more complicated bodies correctly.

--- thunderdome/tests/groovy/test.groovy
+++ thunderdome/tests/groovy/test.groovy
@@ -10,3 +10,14 @@ def second_method(a1, a2, a3) {
 def get_self(eid) {
 	g.v(eid)
 }
+
+def long_func(eid) {
+	t = g.startTransaction()
+	try {
+	  if (something) {
+	    g.e(e)
+	  }
+	} catch(e) {
+	  throw e
+	}
+}
