eb915f779d160a2160ec8c87422248422e22fc64
Jon Haddad <jon@grapheffect.com>
2013-01-15 16:13:26 -0800

    fixed graph name, back to thunderdome for testing

--- thunderdome/tests/base.py
+++ thunderdome/tests/base.py
@@ -8,7 +8,7 @@ class BaseCassEngTestCase(TestCase):
     def setUpClass(cls):
         super(BaseCassEngTestCase, cls).setUpClass()
         if not connection._hosts:
-            connection.setup(['localhost'], 'grapheffect')
+            connection.setup(['localhost'], 'thunderdome')
 
     def assertHasAttr(self, obj, attr):
         self.assertTrue(hasattr(obj, attr), 
