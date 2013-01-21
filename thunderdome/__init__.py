68fe21b07cc82b58df0ffc3fc014a7db9977bc7a
Eric Scrivner <eric@grapheffect.com>
2013-01-21 12:17:55 -0800

    Import PaginatedVertex

--- thunderdome/__init__.py
+++ thunderdome/__init__.py
@@ -1,6 +1,6 @@
 from thunderdome.columns import *
 from thunderdome.exceptions import *
-from thunderdome.models import Vertex, Edge
+from thunderdome.models import PaginatedVertex, Vertex, Edge
 from thunderdome.gremlin import BaseGremlinMethod, GremlinMethod, GremlinValue, GremlinTable
 from thunderdome.containers import Table
 
