53b6825b07d5a0bb49570165d39e76077e24b1f0
Eric Scrivner <eric@grapheffect.com>
2013-02-18 11:41:06 -0800

    Version Bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #python setup.py register
 #python setup.py sdist upload
 
-version = "0.2.8"
+version = "0.2.9"
 
 long_desc = """
 thunderdome is an Object-Graph Mapper (OGM) for Python
